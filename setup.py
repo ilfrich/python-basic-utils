593871335c1b07bb2820c5b09627ecb001185279
Peter Ilfrich <peter.ilfrich@au1.ibm.com>
2019-11-08 12:04:53 +1100

    Added readme as long description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="pbu",
       version="0.3.8",
       description="Basic Utility module for the Python programming language",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/ilfrich/python-basic-utils",
       author="Peter Ilfrich",
       author_email="das-peter@gmx.de",
