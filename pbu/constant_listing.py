4803ccc51bbe2e959b2b6f02de279f67f332a791
ilfrich <das-peter@gmx.de>
2024-09-05 10:19:24 +1000

    Added some more utility to constant listing

--- pbu/constant_listing.py
+++ pbu/constant_listing.py
@@ -1,4 +1,5 @@
 import inspect
+from typing import Any, List, Optional
 
 
 class ConstantListing:
@@ -8,5 +9,21 @@ class ConstantListing:
         attributes = inspect.getmembers(provided_class, lambda a: not (inspect.isroutine(a)))
         return [a[0] for a in attributes if not (a[0].startswith('__') and a[0].endswith('__'))]
 
-    def get_all_values(self):
+    def get_all_values(self) -> List[Any]:
         return list(map(lambda x: getattr(self, x), self.get_all()))
+
+    def get(self, key: str) -> Optional[Any]:
+        if key not in self.get_all():
+            return None
+        return getattr(self, key)
+
+    def reverse_lookup(self, value: Any) -> Optional[str]:
+        if value not in self.get_all_values():
+            return None
+        
+        keys = self.get_all()
+        for key in keys:
+            if getattr(self, key) == value:
+                return key
+            
+        return None  # should not get here
